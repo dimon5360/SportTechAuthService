eabd7ad41c84c1e6d01fce053e1723e6ce85cdb2
Dmitry <dimon5360@gmail.com>
2023-11-15 00:47:10 +0300

    compile proto + establish grpc connection

--- build_proto.sh
+++ build_proto.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-# protoc --version
+unameOut=$(uname -a)
+case "${unameOut}" in
+    Darwin*)    protoc=protoc;;
+    MINGW*)     protoc=D:/projects/protoc/bin/protoc.exe;;
+    *Msys)      protoc=D:/projects/protoc/bin/protoc.exe;;
+esac
 
+${protoc} --version
+
+go get google.golang.org/grpc
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
 export PATH="$PATH:$(go env GOPATH)/bin"
 
-rm -rf grpc/*.pb.go
+rm -rf proto/*.pb.go
 
-protoc --go_out=. --go_opt=paths=source_relative \
+
+${protoc} --go_out=. --go_opt=paths=source_relative \
   --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-  grpc/*.proto
+  proto/*.proto
--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-	"app/main/storage"
+	"app/main/proto"
 	"app/main/utils"
+	"context"
 	"fmt"
+	"net"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 /// TODO:
-/// 1. write and compile protobuf
-/// 2. include grpc
-/// 3. receive and process queries and transfer responses
-/// 4. connect to kafka
+/// 1. handle request with dbms
+/// 2. transfer response
+
+type AuthUsersService struct {
+	proto.UnimplementedAuthUsersServiceServer
+}
 
 func main() {
 
@@ -20,9 +28,32 @@ func main() {
 
 	fmt.Println("Auth service v." + env.Value("VERSION_APP"))
 
-	storage := storage.CreateStorage()
-	storage.Init(env.Value("CONNECTION_STRING") + "?sslmode=disable")
+	// storage := storage.CreateStorage()
+	// storage.Init(env.Value("CONNECTION_STRING") + "?sslmode=disable")
 
-	user := storage.GetUserById("1")
-	fmt.Println(user)
+	// user := storage.GetUserById("1")
+	// fmt.Println(user)
+
+	lis, err := net.Listen("tcp", "localhost:40402")
+	if err != nil {
+		panic(err)
+	}
+	var opts []grpc.ServerOption
+
+	serv := AuthUsersService{}
+	grpcServer := grpc.NewServer(opts...)
+	proto.RegisterAuthUsersServiceServer(grpcServer, &serv)
+	grpcServer.Serve(lis)
+}
+
+func (s *AuthUsersService) GetUser(ctx context.Context, req *proto.GetUserRequest) (*proto.User, error) {
+
+	return &proto.User{
+		Id:        "1",
+		Username:  "default",
+		Password:  "default",
+		Email:     "default",
+		CreatedAt: timestamppb.New(time.Now()),
+		UpdatedAt: timestamppb.New(time.Now()),
+	}, nil
 }
